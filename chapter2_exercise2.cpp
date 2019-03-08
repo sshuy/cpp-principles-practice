c4753ac02314fcd106594022cedfa7b454a8700a
Sean <sshuy@protonmail.ch>
2019-03-08 18:10:29 +0000

    Add files via upload

---
+++ chapter2_exercise2.cpp
@@ -0,0 +1,34 @@
+// A program on how to get to the bathroom, written with instructions that a computer could follow. Very step by step.
+
+//Starting point is sat at a table. End point is a bathroom upstairs on the left.
+
+#include "../../../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Sat at a table in the restaurant, currently eating a steak. You need to go to the bathroom. You think like a computer how do you instruct yourself there?\n\n";
+	cout << "Chew steak.\n";
+	cout << "Swallow steak.\n";
+	cout << "Place knife and fork down on table.\n";
+	cout << "Push chair out from underneath the table.\n";
+	cout << "Stand up from the chair and walk away from the table.\n";
+	cout << "Look for a employee.\n";
+	cout << "Walk towards employee.\n";
+	cout << "Speak to the employee.\n";
+	cout << "Ask where the bathroom is. (Upstairs on the left!)\n";
+	cout << "Ask where the stairs are.\n";
+	cout << "Start walking towards the stairs.\n";
+	cout << "Acknowledge each restaurant object/employee and avoid bumping into them.\n";
+	cout << "Stop at the stairs.\n";
+	cout << "Start walking and lift each foot up and climb each step at a time.\n";
+	cout << "Stop climbing and walking at the top of the stairs.\n";
+	cout << "Look to the forward and left and check how many doors are on the left.\n";
+	cout << "Start walking forward and continue to look left.\n";
+	cout << "Stop and check first door on the left.\n";
+	cout << "Acknowledge the infomation on the door. (Men's Bathroom)\n";
+	cout << "Place hands on door and push.\n";
+	cout << "Walk forward through the door whilst pushing.\n";
+
+	keep_window_open();
+	return 0;
+}
